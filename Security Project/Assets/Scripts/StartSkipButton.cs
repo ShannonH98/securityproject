05e8e15984a7e1e44ee99cf719e07fd8fb2fba42
KingToast98 <32875984+ShannonH98@users.noreply.github.com>
2020-04-13 10:47:26 -0500

    Update to project

---
+++ Security Project/Assets/Scripts/StartSkipButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class StartSkipButton : MonoBehaviour
+{
+    public string areaToLoad;
+    // Start is called before the first frame update
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(areaToLoad);
+    }
+}
